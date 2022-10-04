b1c12ff6f1c5c1fa29e087d66d42bb65d4daceee
tuan <jhmh0226@gmail.com>
2022-10-04 18:11:07 +0900

    20221004

--- chap01_01_grammar1/05_if_else_score.py
+++ chap01_01_grammar1/05_if_else_score.py
@@ -1,9 +1,37 @@
 
+def print_score(score):
+    if score >= 95:
+        grade = "A+"
+    elif score >= 90:
+        grade = "A0"
+    elif score >= 85:
+        grade = "B+"
+    elif score >= 80:
+        grade = "B0"
+    elif score >= 75:
+        grade = "C+"
+    elif score >= 70:
+        grade = "C0"
+    elif score >= 65:
+        grade = "D+"
+    elif score >= 60:
+        grade = "D0"
+    else:
+        grade = "F"
+    return grade
+
+
+def exist_fruit(fruit):
+    fruit_list = ['사과', '딸기', '배', '오렌지', '바나나', '파인애플']
+    if fruit in fruit_list:
+        print("{0}은 과일 목록에 존재합니다.".format(fruit))
+    else:
+        print("{0}은 과일 목록에 존재하지 않습니다.".format(fruit))
+
 if __name__ == "__main__" :
 
-    a = 230
+   #score = input("점수 : ")
+   #print("학점 : " + print_score(int(score)))
 
-    if a > 200:
-        print('200보다 큽니다.')
-    else :
-        print("200보다 작습니다.")
+   fruit = input("과일 : ")
+   exist_fruit(fruit)
---
+++ chap01_01_grammar1/06_for_while_loop_01.py
@@ -0,0 +1,4 @@
+
+if __name__ == "__main__":
+    for i in range(0, 3, 1):
+        print("안녕하세요.", i)
