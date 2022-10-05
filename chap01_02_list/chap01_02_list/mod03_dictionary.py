143157a64e0550d99cd9de21b78c24706c74f4dc
tuan <jhmh0226@gmail.com>
2022-10-05 18:13:45 +0900

    20221005

--- chap01_02_list/chap01_02_list/mod03_dictionary.py
+++ chap01_02_list/chap01_02_list/mod03_dictionary.py
@@ -19,7 +19,26 @@ if __name__ == '__main__':
     print(dic1['age'])
     print()
 
+    # get 사용 시 키값이 없으면 값을 가져오지 않는다.(None 값을 가지고 오며, 오류가 발생하지 않는다.)
     print(dic1.get('id'))
     print(dic1.get('name'))
     print(dic1.get('department'))
     print(dic1.get('age'))
+    print()
+
+
+    singer = {'이름':'트와이스', '구성원수':9, '데뷔':'서바이벌 식스틴', '대표곡':'시그널'}
+    # 딕셔너리.keys() 함수 : 딕셔너리의 모든 키를 반환
+    for k in singer.keys():
+        print('{0} : {1}'.format(k, singer.get(k)))
+    print()
+
+    # 딕셔너리.values() 함수 : 딕셔너리의 모든 값을 반환
+    for value in list(dic.values()):
+        print("{0}".format(value))
+    print()
+
+    # 딕셔너리.items() 함수 : 딕셔너리의 모든 (키, 값)을 반환
+    for (key, value) in list(dic.items()):
+        print(f'{key} --> {value}')
+    print()
